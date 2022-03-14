75651f030657f50211fb9e8e62ae33fced9203f1
Xavier Silva <Xavier.Silva20@kzoo.edu>
2022-03-14 15:59:40 -0400

    Add resetting of voter data.

--- python/init_orgs.py
+++ python/init_orgs.py
@@ -4,6 +4,10 @@ from main import CLA, CTF
 CLA = CLA()
 CTF = CTF()
 
+# reset voting data
+CLA.saveVoters(True)
+CTF.saveVoteTally(True)
+
 # storing variables in files relies on a folder called 'variable_files' existing first
 if not os.path.exists('variable_files'):
     os.mkdir('variable_files')
